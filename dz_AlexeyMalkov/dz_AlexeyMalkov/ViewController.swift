25323e0f366a1c5c6763f0b44625469154bf6721
Alexey Malkov <marlowe.wind@gmail.com>
2020-04-25 21:47:49 +0300

    Добавил интерфейс логина

---
+++ dz_AlexeyMalkov/dz_AlexeyMalkov/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  dz_AlexeyMalkov
+//
+//  Created by Алексей Мальков on 25.04.2020.
+//  Copyright © 2020 Alexey Malkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var authorizationLabel: UILabel!
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+}
+
