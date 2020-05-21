07e277709d308fea332bf2ed62bbe127577c13df
Alexey Malkov <marlowe.wind@gmail.com>
2020-05-21 18:11:24 +0300

    На проверк (5ое задание)

--- dz_AlexeyMalkov/dz_AlexeyMalkov/UsersFriends/FirstTabTableViewController.swift
+++ dz_AlexeyMalkov/dz_AlexeyMalkov/UsersFriends/FirstTabTableViewController.swift
@@ -16,23 +16,48 @@ class FirstTabTableViewController: UITableViewController {
         UserList(name: "Юрий", avatar: UIImage(named: "3")!, userImage: UIImage(named: "23")!),
         UserList(name: "Зоя", avatar: UIImage(named: "4")!, userImage: UIImage(named: "24")!)
     ]
+    var userIndex = ["Л", "З", "С", "Ю"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
-    // MARK: - Table view data source
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return userIndex.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return userIndex[section]
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return userIndex
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return user.count
+        var userRow = [UserList]()
+        for a in user{
+            if userIndex[section].contains(a.name.first!){
+            userRow.append(a)
+            }
+        }
+        return userRow.count
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let passedImage = user[indexPath.row].userImage
+        
+        var userRow = [UserList]()
+        for a in user{
+            if userIndex[indexPath.section].contains(a.name.first!){
+            userRow.append(a)
+            }
+        }
+        
+        let passedImage = userRow[indexPath.row].userImage
         performSegue(withIdentifier: "fromTableToCollection", sender: passedImage)
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "fromTableToCollection" {
             if let destinationVC = segue.destination as? UsersPhotoCollectionViewController, let passedImage = sender as? UIImage {
@@ -43,12 +68,18 @@ class FirstTabTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "userInfoCell", for: indexPath) as! friendsTableViewCell
-        cell.avatarImageView.image = user[indexPath.row].avatar
-        cell.nameLabel.text = user[indexPath.row].name
+        
+        var userRow = [UserList]()
+        for a in user{
+            if userIndex[indexPath.section].contains(a.name.first!){
+            userRow.append(a)
+            }
+        }
+        
+        cell.avatarImageView.image = userRow[indexPath.row].avatar
+        cell.nameLabel.text = userRow[indexPath.row].name
         
         return cell
     }
-
-
-
+    
 }
